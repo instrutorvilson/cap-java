14816029f85c8e9f7bb224987a7852da7d4d8617
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-16 13:59:25 -0300

    Metodos construtores

--- POO/src/Poo.java
+++ POO/src/Poo.java
@@ -11,13 +11,23 @@ public class Poo {
 		obj2.nome = "Joana";
 		obj2.email = "joana@gmail.com";*/
 		
-		obj.setNome("maria");
+		/*obj.setNome("maria");
 		obj.setEmail("maria@gmail.com");
+		obj.setCpf("12345678912");
 		
 		System.out.println(obj.getNome());
 		System.out.println(obj.getEmail());
+		System.out.println(obj.getCpf());
+		
+		Pessoa obj3 = new Pessoa("Jose");
+		System.out.println(obj3.getNome());
+
+		 */
+		Pessoa obj4 = new Pessoa("Janete","janete@gmail.com","12378923561");
+		System.out.println(obj4.getNome());
+		System.out.println(obj4.getEmail());
+		System.out.println(obj4.getCpf());
 	}
 
 }
-
 //encapsulamento -> proteger um recurso: atributo, método
--- POO/src/entidades/Pessoa.java
+++ POO/src/entidades/Pessoa.java
@@ -1,31 +1,56 @@
 package entidades;
 
 public class Pessoa {
-   private String nome;
-   private String email; 
-   
-   public void setNome(String nome) {
-	   this.nome = nome;
-   }
-   
-   public void setEmail(String email) {
-	   if(email.contains("@")) {
-		   this.email = email;  
-	   }
-	   else {
-		   System.out.println("Email inválido!");
-	   }	   
-   }
-   
-   public String getNome() {
-	   return this.nome;
-   }
-   
-   public String getEmail() {
-	   return this.email;
-   }
+	private String nome;
+	private String email;
+	private String cpf;
+
+	public Pessoa(){
+		//System.out.println("chamou construtor");
+	}
+	
+	public Pessoa(String nome) {
+	   this.nome = nome;	
+	}
+	
+	public Pessoa(String nome, String email, String cpf) {
+		this.nome = nome;
+		this.setEmail(email);
+		this.setCpf(cpf);
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public void setCpf(String cpf) {
+		if(cpf.length() == 11) {
+		   this.cpf = cpf;
+		}else {
+			System.out.println("cpf inválido!");
+		}
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setEmail(String email) {
+		if (email.contains("@")) {
+			this.email = email;
+		} else {
+			System.out.println("Email inválido!");
+		}
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
 }
 
 //modificador public -> fica visivel em todas as classes que instanciarem a classe
 //modificador private -> fica visivel somente dentro da classe
-
