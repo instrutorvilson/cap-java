d426310d924b1a96573f853d79dd9c42b7d9198e
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-16 11:47:57 -0300

    Coleção hashset

--- Aula1/src/aula1/Aula.java
+++ Aula1/src/aula1/Aula.java
@@ -2,6 +2,7 @@ package aula1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,9 +11,27 @@ public class Aula {
 	public static void main(String[] args) {
 		//arrayList();
 		//listaLinkada()
+		//listaHashSet();
+		haskMapList();
 	}
 
+	public static void listaHashSet() {
+		//não aceita valores duplicados
+		HashSet<String> cars = new HashSet<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("BMW");
+	    cars.add("Mazda");
+	      
+	    for(String car : cars) {
+	    	System.out.println(car);
+	    }
+	    
+	    
+	}
 	public static void listaLinkada() {
+		//armazenar os valores em container
 		LinkedList<String> cars = new LinkedList<String>();
 		cars.add("Volvo");
 		cars.add("BMW");
@@ -24,6 +43,8 @@ public class Aula {
 	}
 	
 	public static void arrayList() {
+		//simula o array
+		//ao inserir recria o array original acrescentando a nova posição
 		List<String> cars = new ArrayList<String>();
 	    cars.add("Volvo");
 	    cars.add("BMW");
@@ -36,6 +57,7 @@ public class Aula {
 		 HashMap<String, String> capitais = new HashMap<String, String>();
 		 capitais.put("sc", "Florianopolis");
 		 capitais.put("pr", "Curitiba");
+		 capitais.put("pr", "São Jose do Pinhais");
 		 capitais.put("rs", "Porto Alegre");
 		 
 		 for (String key : capitais.keySet()) {
