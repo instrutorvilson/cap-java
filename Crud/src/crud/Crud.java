6fdbb6d5413cf6eae771680dd203b88fb459eea2
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-17 10:50:52 -0300

    Ajustes crud

--- Crud/src/crud/Crud.java
+++ Crud/src/crud/Crud.java
@@ -1,5 +1,7 @@
 package crud;
 
+import java.util.List;
+
 public class Crud {
 
 	public static void main(String[] args) {
--- Crud/src/dao/DaoProduto.java
+++ Crud/src/dao/DaoProduto.java
@@ -3,8 +3,41 @@ package dao;
 import java.util.List;
 
 import entidades.Produto;
-import interfaces.IDAOProduto;
+import interfaces.IDAO;
 
+public class DaoProduto implements IDAO<Produto> {
+
+	@Override
+	public boolean salvar(Produto obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Produto getOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produto> getTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean alterar(Produto obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}}
+
+/*
 public class DaoProduto implements IDAOProduto {
 
 	@Override
@@ -38,4 +71,4 @@ public class DaoProduto implements IDAOProduto {
 	}
 
 	
-}
+}*/
