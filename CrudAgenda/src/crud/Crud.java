700a23446c97910898cf208b7fda8c24fb1b7f79
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-17 13:53:03 -0300

    Criar método salvar

--- CrudAgenda/src/crud/Crud.java
+++ CrudAgenda/src/crud/Crud.java
@@ -1,12 +1,18 @@
 package crud;
 
-import utilidades.Conexao;
+import dao.DaoContato;
+import entidades.Contato;
 
 public class Crud {
 
 	public static void main(String[] args) {
-	  	if (Conexao.conectar() != null) {
-	  		System.out.println("conectado");
+	  	Contato ct = new Contato();
+	  	ct.setNome("Maria");
+	  	ct.setEmail("maria@gmail.com");
+	  	
+	  	DaoContato dc = new DaoContato();
+	  	if(dc.salvar(ct)) {
+	  		System.out.println("Contato salvo com sucesso.");
 	  	}
 	}
 
--- CrudAgenda/src/dao/DaoContato.java
+++ CrudAgenda/src/dao/DaoContato.java
@@ -1,16 +1,30 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import entidades.Contato;
 import interfaces.IDAO;
+import utilidades.Conexao;
 
 public class DaoContato implements IDAO<Contato>{
 
 	@Override
 	public boolean salvar(Contato obj) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "insert into contato(nome, email)values(?,?)";
+		Connection con = Conexao.conectar();
+		try {
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setString(1, obj.getNome());
+			stm.setString(2, obj.getEmail());
+			stm.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		return true;
 	}
 
 	@Override
