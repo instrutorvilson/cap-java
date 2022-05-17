c90b8800e26e8d9d93ae4bb5cb98f3e5754a0d29
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-17 10:50:11 -0300

    Tratando exceções

---
+++ TratamentoExcecao/src/tratamentoexcecao/TratamentoExcecao.java
@@ -0,0 +1,46 @@
+package tratamentoexcecao;
+
+import javax.swing.JOptionPane;
+
+import entidades.Contato;
+
+public class TratamentoExcecao {
+
+	public static void main(String[] args) {
+		Contato ct = new Contato();
+		int x = 10;
+		int y = 2;
+		try {
+			int[] numeros = { 10, 20, 30 };
+			System.out.println(numeros[2]); 
+			
+			int z = x / y;
+			System.out.println(z);	
+			
+			ct.setNome("maria");
+			System.out.println(ct.getNome());
+			
+			String speso = JOptionPane.showInputDialog("Informe peso");
+			float peso = Float.parseFloat(speso);
+			
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Não é possivel dividir por zero");
+			System.out.println("tostring: " + e.toString());
+			System.out.println("Message: " + e.getMessage());
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Posição inválida");
+		}
+		
+		catch (NullPointerException e) {
+			System.out.println("Exceção na execuçaõ do programa: " + e.toString());
+		}
+		
+		catch(NumberFormatException e) {
+			System.out.println("Entrada incorreta: " + e.getMessage());
+		}
+	}
+
+}
