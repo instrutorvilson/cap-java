58b9f5a891ec0e6070da7df62595f96c3d1bbb21
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-18 14:30:46 -0300

    Criado cadastro de contato

--- AgendaContato/src/main/java/com/aulas/mvc/controller/ContatoController.java
+++ AgendaContato/src/main/java/com/aulas/mvc/controller/ContatoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aulas.mvc.dao.DaoContato;
@@ -15,7 +17,7 @@ import com.aulas.mvc.entidades.Contato;
 public class ContatoController {
    
 	@GetMapping("/cadastro")
-	public String cadastrarContato(Model model) {
+	public String formContato(Model model) {
 		model.addAttribute("contato", new Contato());
 		return "cadastro";
 	}
@@ -26,5 +28,13 @@ public class ContatoController {
 		
 		model.addAttribute("lista", lista);
 		return "consulta";
+	}
+	
+	@PostMapping("/cadastro")
+	public String cadastrarContato(@ModelAttribute Contato contato) {
+		if(new DaoContato().salvar(contato)) {
+			return "redirect:/contato/consulta";	
+		}
+		return "cadastro";
 	}
 }
