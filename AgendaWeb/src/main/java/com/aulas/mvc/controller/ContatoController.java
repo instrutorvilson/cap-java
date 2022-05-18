6d4dacd127e5841e6ff69703c6190e83401ff331
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-18 11:10:11 -0300

    Agrupar rotas em um unico controller

--- AgendaWeb/src/main/java/com/aulas/mvc/controller/ContatoController.java
+++ AgendaWeb/src/main/java/com/aulas/mvc/controller/ContatoController.java
@@ -1,5 +1,8 @@
 package com.aulas.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,19 @@ public class ContatoController {
 		model.addAttribute("contato", ct);
 		
 		return "consultacontato";
+	}
+	
+	@GetMapping("/consulta")
+	public String listaContatos(Model model) {
+		List<Contato> lista = new ArrayList<>();
+		lista.add(new Contato(1,"Maria","maria@gmail.com"));
+		lista.add(new Contato(2,"Joana","joana@gmail.com"));
+		lista.add(new Contato(1,"Carla","carla@gmail.com"));
+		lista.add(new Contato(1,"Ursula","ursula@gmail.com"));
+		
+		model.addAttribute("lista", lista);
+		
+		return "tabelacontatos";
 	}
 	
 }
