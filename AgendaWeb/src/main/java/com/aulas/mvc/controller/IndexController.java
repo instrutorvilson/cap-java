7301c7da7a980358a5ddb7bb4894366b3971f944
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-18 11:54:39 -0300

    Criar tela de cadastro de contatos

--- AgendaWeb/src/main/java/com/aulas/mvc/controller/IndexController.java
+++ AgendaWeb/src/main/java/com/aulas/mvc/controller/IndexController.java
@@ -3,7 +3,11 @@ package com.aulas.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aulas.mvc.entidades.Contato;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +17,16 @@ public class IndexController {
 	public String irIndex(Model model) {
 		model.addAttribute("nome","Jose da silva");
 		return "index";
+	}
+	
+	@GetMapping("/cadastro")
+    public String criarUsuario(Model model) {
+    	model.addAttribute("contato", new Contato(1,"Pedro","pedro@gmail.com"));
+    	return "cadastro";
+    }
+	
+	@PostMapping("/cadastro")
+	public String mostrarContatoCadastrado(@ModelAttribute Contato contato) {
+	   return "viewcontato";
 	}
 }
