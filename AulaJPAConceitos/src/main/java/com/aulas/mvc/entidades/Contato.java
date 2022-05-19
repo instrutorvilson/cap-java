415755de91e815b11a8b8e0aa5319412fd384249
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-19 14:13:08 -0300

    Criação de projeto JPA e rodar script de inserção

---
+++ AulaJPAConceitos/src/main/java/com/aulas/mvc/entidades/Contato.java
@@ -0,0 +1,50 @@
+package com.aulas.mvc.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contato {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String email;
+		
+	public Contato() {
+	
+	}
+
+	public Contato(int id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ AulaJPAConceitos/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into contato(nome, email) values ('maria','maria@gmail.com');
+insert into contato(nome, email) values ('ana','ana@gmail.com');
+insert into contato(nome, email) values ('josefa','josefa@gmail.com');
+insert into contato(nome, email) values ('ursula','ursula@gmail.com');
