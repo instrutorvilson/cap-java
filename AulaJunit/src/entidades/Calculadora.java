b39575e62e2fa9878c2fe5fb139403901662352c
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-24 15:58:33 -0300

    Teste de funcionalidade de divisão com dois cenarios

--- AulaJunit/src/entidades/Calculadora.java
+++ AulaJunit/src/entidades/Calculadora.java
@@ -13,10 +13,10 @@ public class Calculadora {
 		return n1 * n2;
 	}
 
-	public double dividir(double n1, double n2) throws Exception{
-		if( n2 <= 0) {
+	public double dividir(double dividendo, double divisor) throws Exception{
+		if( divisor <= 0) {
 			throw new Exception("Não é possivel dividir por zero.");
 		}
-		return n1 / n2;
+		return dividendo / divisor;
 	}
 }
--- AulaJunit/src/testes/CalculadoraTest.java
+++ AulaJunit/src/testes/CalculadoraTest.java
@@ -6,18 +6,43 @@ import org.junit.jupiter.api.Test;
 import entidades.Calculadora;
 
 public class CalculadoraTest {
-  //AAA
+	// AAA
 	@Test
 	public void resultaDeveSerDezQuandoSomarCincoComCinco() {
-		//Arrange
+		// Arrange
 		Calculadora calculadora = new Calculadora();
-		
-		//Act
+
+		// Act
 		double resultado = calculadora.somar(5.0, 5.0);
 		double valorEsperado = 10.0;
-		
-		//Assert
+
+		// Assert
 		Assertions.assertEquals(valorEsperado, resultado);
 	}
+
+	@Test
+	public void resultadoDivisaoOkQuandoDivisorDiferenteZero() throws Exception {
+		// Arrange
+		Calculadora calculadora = new Calculadora();
+		double divisor = 10.0;
+		double dividendo = 20.0;
+
+		// Act
+		double valorEsperado = 2.0;
+		double valorRetornado = calculadora.dividir(dividendo, divisor);
+		// Assertions
+		Assertions.assertEquals(valorEsperado, valorRetornado);
+	}
 	
+    @Test
+	public void divisaoLancaExcecaoQuandoDivisorZero() {
+		// Arrange   100/0
+		Calculadora calculadora = new Calculadora();
+		double divisor = 0.0;
+		double dividendo = 10.0;
+
+		// Act
+		Exception ex = Assertions.assertThrows(Exception.class, () -> calculadora.dividir(dividendo, divisor));
+		Assertions.assertTrue(ex.getMessage().contains("Não é possivel dividir por zero."));
+	}
 }
