03047b56f01bbcc82b68077bf1f8efe880dd322a
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-24 16:17:24 -0300

    Teste de funcionalidade de multiplicação e subtração

--- AulaJunit/src/testes/CalculadoraTest.java
+++ AulaJunit/src/testes/CalculadoraTest.java
@@ -6,7 +6,29 @@ import org.junit.jupiter.api.Test;
 import entidades.Calculadora;
 
 public class CalculadoraTest {
-	// AAA
+	@Test
+	public void resultadoOkQuandoDiminuirDoisNumeros() {
+		Calculadora calculadora = new Calculadora();
+		double n1 = 50;
+		double n2 = 100;
+		
+		double valorEsperado = -50;
+		double valorRetornado = calculadora.subtrair(n1, n2);
+		Assertions.assertEquals(valorEsperado, valorRetornado);
+	}
+	
+	@Test
+	public void resultadoOkQuandoMultiplicarDoisNumeros() {
+		Calculadora calculadora = new Calculadora();
+		double valor = 100.0;
+		double multiplicador = 2.0;
+		
+		double valorEsperado = 200.0;
+		double valorRetornado = calculadora.multiplicar(valor, multiplicador);
+		
+		Assertions.assertEquals(valorEsperado, valorRetornado);
+	}
+	
 	@Test
 	public void resultaDeveSerDezQuandoSomarCincoComCinco() {
 		// Arrange
@@ -36,6 +58,7 @@ public class CalculadoraTest {
 	
     @Test
 	public void divisaoLancaExcecaoQuandoDivisorZero() {
+
 		// Arrange   100/0
 		Calculadora calculadora = new Calculadora();
 		double divisor = 0.0;
