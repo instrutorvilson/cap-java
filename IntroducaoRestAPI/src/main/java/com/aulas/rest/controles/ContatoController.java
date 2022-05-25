54bbce8604f8755557847dcdb7a792b065be4d18
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-25 10:44:14 -0300

    Criação de rota inserir contato

--- IntroducaoRestAPI/src/main/java/com/aulas/rest/controles/ContatoController.java
+++ IntroducaoRestAPI/src/main/java/com/aulas/rest/controles/ContatoController.java
@@ -1,15 +1,31 @@
 package com.aulas.rest.controles;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aulas.rest.entidades.Contato;
 
 @RestController
 @RequestMapping("/contato")
 public class ContatoController {
   
 	@GetMapping
-	public String ola() {
-		return "ola mundo";
+	public ResponseEntity<String> ola() {
+		return ResponseEntity.ok("ola mundo");
+	}
+	
+	@GetMapping("/{nome}")
+	public String ola(@PathVariable("nome") String nome) {
+		return "ola " + nome;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Contato> inserir(@RequestBody Contato ct){
+		return ResponseEntity.ok(ct);
 	}
 }
