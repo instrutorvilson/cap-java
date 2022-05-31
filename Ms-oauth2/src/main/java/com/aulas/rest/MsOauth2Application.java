24f53442cceef8506a3419690540927ccfb40de3
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-31 13:18:44 -0300

    Projeto de geraçaõ de token oauth2 criado

---
+++ Ms-oauth2/src/main/java/com/aulas/rest/MsOauth2Application.java
@@ -0,0 +1,13 @@
+package com.aulas.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauth2Application.class, args);
+	}
+
+}
