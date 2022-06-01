09e990ef442f89b46b2926ac2dfc9da9b6cbff55
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-01 10:39:42 -0300

    ALterar inserções e criar classe para liberar recursos

---
+++ Ms-oauth2/src/main/java/com/aulas/rest/configuracao/ResourceServerConfig.java
@@ -0,0 +1,57 @@
+package com.aulas.rest.configuracao;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	private static final String[] PUBLIC = { "/oauth/token","/h2-console/**","/usuario/**"};
+	
+	private static final String[] OPERADOR_OR_ADMIN = { "/usuario/**" };
+	
+	private static final String[] ADMIN = { "/usuario/**" };
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		//h2-console
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERADOR_OR_ADMIN).permitAll()
+		.antMatchers(OPERADOR_OR_ADMIN).hasAnyRole("OPERATOR","ADMIN")
+		.antMatchers(ADMIN).hasRole("ADMIN")
+		.anyRequest().authenticated();		
+	}	
+}
