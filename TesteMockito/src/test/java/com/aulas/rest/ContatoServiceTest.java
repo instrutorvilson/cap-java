d2a13430b30762a7ff6d4898353967b206c46bea
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-03 11:16:32 -0300

    Criar testes de pesquisa

--- TesteMockito/src/test/java/com/aulas/rest/ContatoServiceTest.java
+++ TesteMockito/src/test/java/com/aulas/rest/ContatoServiceTest.java
@@ -1,5 +1,7 @@
 package com.aulas.rest;
 
+import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.aulas.rest.entity.Contato;
 import com.aulas.rest.repository.ContatoRepository;
 import com.aulas.rest.service.ContatoService;
 
@@ -19,6 +22,7 @@ import com.aulas.rest.service.ContatoService;
 public class ContatoServiceTest {
     private Long idExistente;
     private Long idInexistente;
+    private Contato contato;
     
 	@InjectMocks
 	private ContatoService service;
@@ -30,9 +34,12 @@ public class ContatoServiceTest {
 	void setup() {
 		idExistente = 1L;
 		idInexistente = 100L;
+		contato = new Contato();
 		
 		Mockito.doNothing().when(repository).deleteById(idExistente);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(idInexistente);
+		Mockito.when(repository.findById(idExistente)).thenReturn(Optional.of(contato));
+		Mockito.doThrow(EntityNotFoundException.class).when(repository).findById(idInexistente);
 	}
 	
 	@Test
@@ -44,11 +51,26 @@ public class ContatoServiceTest {
 	}
 	
 	@Test
-	void levantaExcecaoEmptyResultDataAccessExceptionQuandoDeleteIdInexistente() {
+	void levantaEntityNotFoundExceptionQuandoDeleteIdInexistente() {
 		Assertions.assertThrows(EntityNotFoundException.class, () ->{
 			service.delete(idInexistente);
 		});
 		Mockito.verify(repository, Mockito.times(1)).deleteById(idInexistente);		
 	}
 	
+	@Test
+	void retornaNaoNuloQuandoConsultaPorIdExistente() {
+		//Assertions.assertNotNull(service.pesquisar(idExistente));	
+		Contato c = service.pesquisar(idExistente);
+		Assertions.assertEquals(c, contato);
+		Mockito.verify(repository).findById(idExistente);
+	}
+	
+	@Test
+	void lancaEntityNotFoundExceptionQuandoPesquisaIdInexistente() {
+		Assertions.assertThrows(EntityNotFoundException.class, () -> {
+			service.pesquisar(idInexistente);
+		});
+		Mockito.verify(repository).findById(idInexistente);
+	}
 }
