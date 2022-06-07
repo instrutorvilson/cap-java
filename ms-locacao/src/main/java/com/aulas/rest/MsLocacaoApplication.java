fe8843e3af08622e98cc026cfd1cf36eccf4665c
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-07 14:13:36 -0300

    Criado microservice de locacao

---
+++ ms-locacao/src/main/java/com/aulas/rest/MsLocacaoApplication.java
@@ -0,0 +1,13 @@
+package com.aulas.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLocacaoApplication.class, args);
+	}
+
+}
---
+++ ms-locacao/src/main/java/com/aulas/rest/entity/Locacao.java
@@ -0,0 +1,75 @@
+package com.aulas.rest.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_locacao")
+public class Locacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date data;
+	private Time hora;
+	private int duracao;
+	private Long local;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Time getHora() {
+		return hora;
+	}
+
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+
+	public Long getLocal() {
+		return local;
+	}
+
+	public void setLocal(Long local) {
+		this.local = local;
+	}
+
+	public Locacao() {
+
+	}
+
+	public Locacao(Long id, Date data, Time hora, int duracao, Long local) {
+		this.id = id;
+		this.data = data;
+		this.hora = hora;
+		this.duracao = duracao;
+		this.local = local;
+	}
+
+}
