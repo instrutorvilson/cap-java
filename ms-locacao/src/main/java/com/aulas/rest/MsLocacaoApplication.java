30b72a8ea9a6cff81289a8429eaab5e349703d08
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-08 10:43:16 -0300

    Configurado cliente eureka

--- ms-locacao/src/main/java/com/aulas/rest/MsLocacaoApplication.java
+++ ms-locacao/src/main/java/com/aulas/rest/MsLocacaoApplication.java
@@ -2,9 +2,11 @@ package com.aulas.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class MsLocacaoApplication {
 
